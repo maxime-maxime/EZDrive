de96616af5903a4107937af2ba7fe3ce923a3241
mad_max <maxime.thav2004@gmail.com>
2025-12-05 16:33:03 +0100

    qsd

--- former/analyse.php
+++
@@ -1,8 +0,0 @@
-if(file_exists("requetes.json")){
-    $loadreq=file_get_contents("requetes.json");
-    $requetesjson = json_decode($loadreq);
-    echo $requetesjson->tab1[0];
-}
-else{
-    echo'le fichier json est manquant';
-}
